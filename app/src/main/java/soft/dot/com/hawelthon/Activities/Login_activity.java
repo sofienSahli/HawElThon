b56f457fd46e97bf21ae9e099dd39c1e52d2ed32
sofien <sofien.sahli@esprit.tn>
2018-10-20 22:38:56 +0100

    hahahahaha

--- app/src/main/java/soft/dot/com/hawelthon/Activities/Login_activity.java
+++ app/src/main/java/soft/dot/com/hawelthon/Activities/Login_activity.java
@@ -2,14 +2,20 @@ package soft.dot.com.hawelthon.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import soft.dot.com.hawelthon.R;
 
-public class Login_activity extends AppCompatActivity {
+public class Login_activity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
